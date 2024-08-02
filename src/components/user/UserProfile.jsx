b355919dd59e61817aa5d3342f7a968aba66d477
Pranav <pranav.2002tis@gmail.com>
2024-08-02 13:02:36 +0530

    Minor UI Change

--- src/components/user/UserProfile.jsx
+++ src/components/user/UserProfile.jsx
@@ -209,7 +209,7 @@ const UserProfile = () => {
             </div>
             <Button
               type="submit"
-              className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
+              className="inline-flex items-center px-4 py-1.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
             >
               {isLoading ? (
                 <>
